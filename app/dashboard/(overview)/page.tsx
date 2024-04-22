41a50c4bc41d84ab507af02fdf2161d347331714
superscalar <65932360+superscalar@users.noreply.github.com>
2024-04-21 21:38:43 -0300

    before starting chapter 11

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,17 +1,13 @@
-import { /*fetchRevenue, */fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
-
-import { Card } from '@/app/ui/dashboard/cards';
+import CardWrapper from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 
 import { Suspense } from 'react';
-import { LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { LatestInvoicesSkeleton, RevenueChartSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
 
 // try async in client components later
 export default async function Page() {
-	/* const revenue = await fetchRevenue(); */
-	const cardData = await fetchCardData();
 	
   return (
     <main>
@@ -20,14 +16,18 @@ export default async function Page() {
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
 	  
-        <Card title="Collected" value={cardData.totalPaidInvoices} type="collected" />
+		<Suspense fallback={<CardsSkeleton />}>
+			<CardWrapper />
+		</Suspense>
+	  
+        {/*<Card title="Collected" value={cardData.totalPaidInvoices} type="collected" />
         <Card title="Pending" value={cardData.totalPendingInvoices} type="pending" />
         <Card title="Total Invoices" value={cardData.numberOfInvoices} type="invoices" />
         <Card
           title="Total Customers"
           value={cardData.numberOfCustomers}
           type="customers"
-        />
+        /> */}
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
 	  
