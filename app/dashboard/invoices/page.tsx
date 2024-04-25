06fff8a54d9ac1284029872c4c66a6cc0a97a2b5
superscalar <65932360+superscalar@users.noreply.github.com>
2024-04-25 12:12:35 -0300

    Before chapter 12

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,15 +1,49 @@
-import { Metadata } from 'next';
+import { lusitana } from '@/app/ui/fonts';
 
-export const metadata: Metadata = {
-  title: 'Invoices',
-};
+import { Suspense } from 'react';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 
-export default function Page() {
+import Pagination from '@/app/ui/invoices/pagination';
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/invoices/table';
+
+import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import { fetchInvoicesPages } from '@/app/lib/data'; 
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchInvoicesPages();
+  
   return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        <h1 className="text-white text-[3rem]">Invoices</h1>
+    <div className="w-full">
+	  {/* why not make this a header? semantics are good */}
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
       </div>
-    </main>
+	  
+	  {/* and this isn't a <form> - does this mean react does its own submit-type behaviour? */}
+	  {/* it does, with useRouter's replace */}
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search invoices..." />
+		
+        <CreateInvoice />
+      </div>
+	   
+	  <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense>
+	  
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination currentPage={currentPage} totalPages={totalPages} />
+      </div>
+    </div>
   );
 }
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,16 +1,57 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+	const searchParams = useSearchParams();
+	const pathname = usePathname();
+	const { replace } = useRouter();
+	
+	const handleSearch = useDebouncedCallback((term) => {
+	  console.log(`Searching... ${term}`);
+	 
+	  const params = new URLSearchParams(searchParams);
+	  params.set('page', 1);
+	  
+	  if (term) {
+		params.set('query', term);
+	  } else {
+		params.delete('query');
+	  }
+	  replace(`${pathname}?${params.toString()}`);
+	}, 300);
+	
+	/*
+	const handleSearch = (searchTerm: string) => {
+		console.log(searchTerm);
+		const params = new URLSearchParams(searchParams);
+		if (searchTerm != "") {
+			params.set('query', searchTerm);
+		} else {
+			params.delete('query');
+		}
+		
+		// the original pathname is implicit
+		replace(`${pathname}?${params.toString()}`);
+	};
+	*/
+
+	/* isn't onChange very inefficient? one query per keystroke and even if mistakes happen, backspace resends queries */
+	// it is covered later - they recommend use-debounce, which is solving a problem that they themselves created, but it does work
+	// though it feels like using the normal html controls is better
+	
   return (
     <div className="relative flex flex-1 flex-shrink-0">
-      <label htmlFor="search" className="sr-only">
-        Search
-      </label>
+      <label htmlFor="search" className="sr-only">Search</label>
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+		onChange={(e) => {
+			handleSearch(e.target.value);
+		}}
+		defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
